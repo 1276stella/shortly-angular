33777770263c6743ccc4ec313ba91e8d974df59e
MKS-Student <mks.student@hackreactor.com>
2015-10-14 18:49:21 -0700

    Completed Links And Shorten Controllers

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,19 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, $http, Links) {
   // Your code here
+  $scope.data={};
+
+  $scope.getLinks=function(){
+    $http({
+      method: 'GET',
+      url: '/api/links'
+    })
+    .then(function (resp) {
+      $scope.data.links = resp.data;
+    }, function(err){
+      if(err) throw err;
+    });
+  };
+  $scope.getLinks();
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,27 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location,$http, Links) {
   // Your code here
+  $scope.link={};
+
+  $scope.addLink=function(link){
+    $http({
+      method:'POST',
+      url: '/api/links',
+      data: link
+      })
+    .then(function(resp){
+      console.log(resp.data);
+    });
+
+  };
 });
+
+
+
+//  it('should be able to create new links with addLink()', function () {
+//     $httpBackend.expectPOST("/api/links").respond(201, '');
+//     $scope.addLink();
+//     $httpBackend.flush();
+//   });
+// });
